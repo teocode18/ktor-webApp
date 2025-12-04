e165670c860f5b8d6a4a490643f032b13c88e4d3
Nick Efford <nick.efford@gmail.com>
2025-12-04 11:23:52 +0000

    html-dsl: updated Amper to 0.9.1 and refactored routing

--- html-dsl/src/Routing.kt
+++ html-dsl/src/Routing.kt
@@ -1,4 +1,5 @@
 import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationCall
 import io.ktor.server.html.respondHtmlTemplate
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
@@ -7,15 +8,23 @@ import kotlinx.html.*
 
 fun Application.configureRouting() {
     routing {
-        get("/roll/d{sides}") {
-            val sides: Int by call.parameters
-            val number = dieRoll(sides)
-            call.respondHtmlTemplate(LayoutTemplate()) {
-                titleText { +"Die Roller" }
-                content {
-                    h1 { +"d$sides Die Roll" }
-                    p { +"Result = $number" }
-                }
+        get("/roll/d{sides}") { call.handleDieRoll() }
+    }
+}
+
+private suspend fun ApplicationCall.handleDieRoll() {
+    val sides: Int by parameters
+    val number = dieRoll(sides)
+
+    respondHtmlTemplate(LayoutTemplate()) {
+        titleText { +"Die Roller" }
+        content {
+            h1 { +"d$sides Die Roll" }
+
+            p { +"Result = $number" }
+
+            a("/roll/d${sides}") {
+                +"Roll Again"
             }
         }
     }
